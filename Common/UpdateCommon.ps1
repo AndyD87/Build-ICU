e31cc2143200b45665279157df37be3fba3e9c9e
Andreas <coolcow_tk@web.de>
2017-08-18 18:16:30 +0200

    Scripts  updated

--- Common/UpdateCommon.ps1
+++ Common/UpdateCommon.ps1
@@ -3,9 +3,6 @@ Import-Module "$PSScriptRoot\Git.ps1"
 # Download new sources
 Git-Clone -Source "https://github.com/AndyD87/Powershell-Common.git" -Target "Powershell-Common"
 
-# Remove all available but this and new
-Remove-Item "$PSScriptRoot\*" -Recurse -Force -Exclude "UpdateCommon.ps1, Powershell-Common"
-
 # Copy downloaded sources
 Move-Item "$PSScriptRoot\Powershell-Common\*" ".\" -Force -Exclude "UpdateCommon.ps1"
 
