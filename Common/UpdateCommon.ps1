7647c7e13eeaf549e95a3f47155c09fae82306e0
Andreas <coolcow_ccos@yahoo.com>
2017-08-23 19:35:48 +0200

    Update common scripts with Testing for Portable Apps

--- Common/UpdateCommon.ps1
+++ Common/UpdateCommon.ps1
@@ -24,11 +24,13 @@
 ##
 Import-Module "$PSScriptRoot\Git.ps1"
 
+Git-GetEnv -Mandatory
+
 # Download new sources
 Git-Clone -Source "https://github.com/AndyD87/Powershell-Common.git" -Target "Powershell-Common"
 
 # Copy downloaded sources
-Move-Item "$PSScriptRoot\Powershell-Common\*" ".\" -Force -Exclude "UpdateCommon.ps1"
+Move-Item "$PSScriptRoot\Powershell-Common\*" ".\" -Force
 
 # Remove Folder with new sources
 Remove-Item "$PSScriptRoot\Powershell-Common" -Recurse -Force
